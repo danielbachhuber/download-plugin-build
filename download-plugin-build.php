6dd0dac8e6fa0a2afa357700aea19f8f3f8d49cc
Daniel Bachhuber <daniel@handbuilt.co>
2017-02-10 06:24:59 -0800

    Add a "Download Build" link for every plugin that has a ZIP

--- download-plugin-build.php
+++ download-plugin-build.php
@@ -11,3 +11,26 @@
  *
  * @package         Download_Plugin_Build
  */
+
+add_filter( 'plugin_action_links', function( $links, $plugin ){
+	$plugin_base_dir = dirname( $plugin );
+	if ( in_array( $plugin_base_dir, array( '.', 'download-plugin-build' ), true ) ) {
+		return $links;
+	}
+
+	$builds = glob( WP_PLUGIN_DIR . '/' . $plugin_base_dir . '*.zip' );
+	if ( empty( $builds ) ) {
+		return $links;
+	}
+	$build = array_shift( $builds );
+	$build_url = str_replace( WP_PLUGIN_DIR, plugins_url(), $build );
+	$build_label = sprintf( 'Download %s', basename( $build ) );
+	$new_links = array();
+	foreach( $links as $k => $j ) {
+		$new_links[ $k ] = $j;
+		if ( in_array( $k, array( 'activate', 'deactivate' ), true ) ) {
+			$new_links['download_plugin_build'] = '<a title="' . esc_attr( $build_label ) . '" href="' . esc_url( $build_url ) . '">Download Build</a>';
+		}
+	}
+	return $new_links;
+}, 10, 2);
