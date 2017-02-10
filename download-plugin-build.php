1aee388d1b68a911d41b919f2fc08a3bb727366f
Daniel Bachhuber <daniel@handbuilt.co>
2017-02-10 05:50:33 -0800

    Initial commit

---
+++ download-plugin-build.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Download Plugin Build
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Adds a "Download Plugin" link if the plugin's build is found.
+ * Author:          Daniel Bachhuber
+ * Author URI:      https://danielbachhuber.com
+ * Text Domain:     download-plugin-build
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Download_Plugin_Build
+ */
